edbf9a0ee2577e73006e3f50b2d4ba81746d5843
Ahmed Hassiki <hassiki.services@gmail.com>
2023-02-18 18:47:13 +0100

    checkpoint-react

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import NavScrollExample from './navbar.js';
+import BasicExample from './form.js';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+<NavScrollExample/>
+<br></br>
+<BasicExample></BasicExample>
+    </div>
+  );
+}
+
+export default App;
